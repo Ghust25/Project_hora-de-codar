d44504a97e19a6bf1d78d8a8f5312ef0c2410eed
Ghust25 <andre.uga15@hotmail.com>
2022-10-15 17:02:26 -0300

    Testes - Slider

--- src/app/components/tests/tests.component.ts
+++ src/app/components/tests/tests.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestsComponent implements OnInit {
   htmlString = '<h1> TESTES Abaixo </h1>';
+  slidePoint!: number
+
+  formatLabel(value: number) {
+    if (value >= 1) {
+      return Math.round(value / 1) + '%';
+    }
+    return value;
+  }
 
   constructor() { }
 
